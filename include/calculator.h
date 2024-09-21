951fb7f0d9b1fd6691a77b7418b148fdb339f51d
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-21 14:41:18 +0900

    各計算関数を記述

--- include/calculator.h
+++ include/calculator.h
@@ -1,3 +1,17 @@
 // calculator.h
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
+
+//加算関数プロトタイプ
+int add(int a, int b);
+
+//減算関数プロトタイプ
+int subtract(int a, int b);
+
+// 乗算関数のプロトタイプ
+int multiply(int a, int b);
+
+// 除算関数のプロトタイプ
+int divide(int a, int b);
+
+#endif
