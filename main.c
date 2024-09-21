4b3c27775720b3b397c1511ef6812cb8f020b8f8
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-21 14:58:48 +0900

    ユーザーメニュー表示部実装

--- main.c
+++ main.c
@@ -3,5 +3,19 @@
 int main() {
   //プログラム起動時メッセージ
   printf("電卓アプリケーション");
+
+  void displayMenu() {
+    printf("===== 電卓メニュー =====\n");
+    printf("1. 加算\n");
+    printf("2. 減算\n");
+    printf("3. 乗算\n");
+    printf("4. 除算\n");
+    printf("5. 終了\n");
+    printf("=======================\n");
+    printf("選択してください");
+  }
+
+  displayMenu();
+
   return 0;
 }
