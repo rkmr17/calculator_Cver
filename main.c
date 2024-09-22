1140669f942295f81408cf745d86315cf978097c
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-22 22:06:37 +0900

    細部修正

--- main.c
+++ main.c
@@ -40,7 +40,7 @@ int multiply(int num1, int num2) {
 //除算関数
 int divide(int num1, int num2) {
   if (num2 == 0) {
-    printf("0では割り切れません");
+    printf("0では割り切れません\n");
     return 0;
   }
   return num1 / num2;
