8dda5727387b9d765b3b1a29a80233a7c1dfc5a4
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-22 20:49:44 +0900

    ユーザー選択処理（仮）

--- main.c
+++ main.c
@@ -8,15 +8,40 @@
     printf("3. 乗算\n");
     printf("4. 除算\n");
     printf("5. 終了\n");
-    printf("=======================\n");
-    printf("選択してください");
+    printf("========================\n");
+    printf("選択してください：");
   }
 
 int main() {
   //プログラム起動時メッセージ
-  printf("電卓アプリケーション");
+  printf("電卓アプリケーション\n");
 
   displayMenu();
+
+  //ユーザー選択
+  int choice;
+  scanf("%d", &choice);
+
+  switch (choice) {
+  case 1:
+    //加算
+    break;
+  case 2:
+    //減算
+    break;
+  case 3:
+    //乗算
+    break;
+  case 4:
+    //除算
+    break;
+  case 5:
+    printf("プログラムを終了します。\n");
+    break;
+  default:
+    printf("無効な選択です。\n");
+    break;
+  }
 
   return 0;
 }
